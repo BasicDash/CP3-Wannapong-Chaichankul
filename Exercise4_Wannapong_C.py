cd3d6138acd353573f749d5b85d6e12c3ce7dafc
Wannapong Chaichankul <76259966+BasicDash@users.noreply.github.com>
2021-09-05 21:37:03 +0700

    Add files via upload

---
+++ Exercise4_Wannapong_C.py
@@ -0,0 +1,11 @@
+Foundation_English = 82.3
+General_Business = 75.8
+Intro_Com_Sys = 99.9
+Computer_Programming = 50.0
+
+print("========Your Score=========")
+print("---------------------------")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer Systems :",Intro_Com_Sys)
+print("Computer Programming",Computer_Programming)
